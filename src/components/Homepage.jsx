31bed565ec2ecafe9810ee774b175da540677dd4
Nick Howell <nlhowell12@gmail.com>
2018-06-28 11:06:20 -0400

    modal SuccessMessage will show up if the user has no projects or activities in their state

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-import { RecentActivity, AppHeader, ProjectListing, NavBar, Tasks } from '../components';
+import { RecentActivity, AppHeader, ProjectListing, NavBar, Tasks, SuccessMessage } from '../components';
+import { connect } from 'react-redux';
 
 class Homepage extends Component {
+    state={
+        modal: true
+    }
+
+    changeModal = () => {
+        this.setState({
+            modal: !this.state.modal
+        })
+    }
+    componentDidMount = () => {
+
+    }
     render() {
+        const { modal } = this.state;
+        const { projects, activities } = this.props;
         return (
             <div className="homepage">
+                <React.Fragment>
+                    {(modal && projects.length === 0 && activities.length === 0) && <div className="backdrop"></div>}
+                    {(modal && projects.length === 0 && activities.length === 0) && <SuccessMessage changeModal={this.changeModal}></SuccessMessage>}
+                </React.Fragment>
                 <AppHeader></AppHeader>
                 <RecentActivity></RecentActivity>
                 <ProjectListing></ProjectListing>
@@ -15,4 +34,11 @@ class Homepage extends Component {
     }
 }
 
-export default Homepage
+const mapStateToProps = (state) => {
+    return {
+        projects: state.projects,
+        activities: state.activities
+    }
+}
+
+export default connect(mapStateToProps)(Homepage);
